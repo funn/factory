307a1aea20098135f2c451ec8fe5b8eec4fb4693
Ilya Shupta <funn17@gmail.com>
2015-06-24 21:44:57 +0600

    MonthlyScheduleForm args checking.

--- app/forms.py
+++ app/forms.py
@@ -16,6 +16,8 @@ class OrderDetailForm(forms.ModelForm):
 class MonthlyScheduleForm(forms.Form):
     def __init__(self, *args, **kwargs):
         days = kwargs.pop('days')
+        if days <= 0:
+            raise ValueError('Days should be a positive value more than zero. days={}'.format(days))
         super(MonthlyScheduleForm, self).__init__(*args, **kwargs)
         for day in range(1, days+1):
             self.fields['day_{}'.format(day)] = forms.BooleanField(required=False)
